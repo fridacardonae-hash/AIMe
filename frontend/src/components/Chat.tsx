eb2b33b1e0ae250b3c9cf09fe23815ec1a151a7b
fridacardonae-hash <fridacardonae@gmail.com>
2026-03-16 18:09:16 -0600

    added scroll for the chat

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,19 +1,26 @@
 import React from "react"
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import Message from "./Message"
 
 
 
 export default function Chat() {
 
+
   const [messages, setMessages] = useState<any[]>([])
   const [question, setQuestion] = useState("")
+
+  const messagesEndRef = useRef<HTMLDivElement | null>(null)
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }, [messages])
 
   const API_URL = "https://aime-backend.victoriousmoss-666ea086.centralus.azurecontainerapps.io"
   const askAIMe = async () => {
 
     const userMessage = { role: "user", text: question }
     setMessages(prev => [...prev, userMessage])
+    setQuestion("")
 
     const res = await fetch(`${API_URL}/ask`, {
       method: "POST",
@@ -28,16 +35,17 @@ export default function Chat() {
     const aiMessage = { role: "ai", text: data.answer }
     setMessages(prev => [...prev, aiMessage])
 
-    setQuestion("")
   }
 
   return (
     <div className="chat">
 
       <div className="messages">
+        
         {messages.map((m, i) => (
           <Message key={i} text={m.text} role={m.role} />
         ))}
+        <div ref={messagesEndRef}/>
       </div>
 
       <div className="input-area">
